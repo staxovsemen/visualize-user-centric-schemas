14708badd264fd89a30f1fa9393e9c444ff1aca2
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-09-21 20:25:00 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,68 +1,13 @@
-
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    data = generate_random_data()
-
-def generate_random_data():
-        print(f"Random Number: {item}")
-    main()
-
-
-    for item in data:
-    return data
-def main():    });
-    data.forEach(item => {
-main();
-    return data;
-
-}
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-        console.log("Random Number: " + item);
-    }
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-
-}
-function main() {
-
-import random
-    for item in data:
-    main()
-def generate_random_data():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    return data
-        print(f"Random Number: {item}")
-
-def main():
-    data = generate_random_data()
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    main()
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
 if __name__ == "__main__":
 def main():
+def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+    main()
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
 
     return data
-import random
-    for item in data:        int[] data = new int[10];
 
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-}
-        }
-
-    }
-        for (int item : data) {
